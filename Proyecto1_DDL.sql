71a624f590ffc1e9e4438b8b411695ee0c3cccd5
Osman Emanuel de León García <115298783+Emadlgg@users.noreply.github.com>
2025-03-19 21:34:10 -0600

    Adding First DDL Query

---
+++ Proyecto1_DDL.sql
@@ -0,0 +1,52 @@
+-- Crear la base de datos
+CREATE DATABASE FinanzasPersonales;
+
+-- Usar la base de datos
+USE FinanzasPersonales;
+
+
+-- Crear la tabla Usuario
+CREATE TABLE Usuario (
+    idUsuario SERIAL PRIMARY KEY,  
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) NOT NULL UNIQUE,
+    contraseña VARCHAR(100) NOT NULL
+);
+
+-- Crear la tabla Reporte
+CREATE TABLE Reporte (
+    idReporte SERIAL PRIMARY KEY, 
+    fechaGeneracion DATE NOT NULL,
+    idUsuario INT,
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
+);
+
+-- Crear la tabla Categoría
+CREATE TABLE Categoria (
+    idCategoria SERIAL PRIMARY KEY, 
+    nombre VARCHAR(100) NOT NULL,
+    idUsuario INT,
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
+);
+
+-- Crear la tabla Transaction
+CREATE TABLE Transaction (
+    idTransaction SERIAL PRIMARY KEY, 
+    monto DOUBLE PRECISION NOT NULL,  
+    fecha DATE NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
+    idCategoria INT,
+    idUsuario INT,
+    FOREIGN KEY (idCategoria) REFERENCES Categoria(idCategoria),
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
+);
+
+-- Crear la tabla Presupuesto
+CREATE TABLE Presupuesto (
+    idPresupuesto SERIAL PRIMARY KEY, 
+    limite DOUBLE PRECISION NOT NULL, 
+    idCategoria INT,
+    idUsuario INT,
+    FOREIGN KEY (idCategoria) REFERENCES Categoria(idCategoria),
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
+);
